22bc06f05336171feb19e391f50a7825857eca56
cri <critz2016@alu.uct.cl>
2019-09-08 00:43:46 -0300

    Modificacion App.js

--- pagina/src/App.js
+++ pagina/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link}  from 'react-router-dom'
+import Login from './components/Login'
+import Registro from './components/Registro'
 import './App.css';
 
 class App extends Component {
   render(){
   return (
-    <div></div>
+    <div className="App">
+      <Router>
+        <ul>
+          <li>
+          <Link to="/Login">Iniciar Sesion</Link>
+          </li>
+          <li>
+          <Link to="/CrearUsuario">Crear Usuario</Link>
+          </li>
+        </ul>
+
+        <Route path="/Login" component="Login"></Route>
+        <Route path="/CrearUsuario" component="Registro"></Route>
+      </Router>
+      
+    </div>
   );
   }
 }
