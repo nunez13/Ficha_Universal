fe1af19c3104ba3f42067be75fe8ef05ca6fd86a
Neeff <dpincheira2016@alu.uct.cl>
2019-09-10 12:27:13 -0300

    add: modificacion app.js

--- pagina/src/App.js
+++ pagina/src/App.js
@@ -5,6 +5,8 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput } from 'mdbreact';
+import Login from './components/Login'
+import Registro from './components/Registro'
 
 
 
@@ -12,7 +14,6 @@ class App extends Component {
 
   render(){
   return (
-<<<<<<< HEAD
     <div className="App">
       <Router>
         <ul>
@@ -27,77 +28,6 @@ class App extends Component {
         <Route path="/Login" component={Login}></Route>
         <Route path="/CrearUsuario" component={Registro}></Route>
       </Router>
-      
-=======
-    <div className="bg">
-      <center>
-        <MDBContainer>
-          <MDBRow style={{top:"50%"}}>
-            <MDBCol md='12'>
-              <MDBCard className='card-image' style={{backgroundImage:'url(http://www.wallpaperk.com/wallpapers/envelope-minimal-blue-7837.jpg)', width: '28rem'}}>
-                <div className='text-white rgba-stylish-strong py-5 px-5 z-depth-4'>
-                  <div className='text-center'>
-                    <h3 className='white-text mb-5 mt-4 font-weight-bold'>
-                      <strong>SIGN</strong>
-                      <strong className='green-text font-weight-bold'> UP</strong>
-                    </h3>
-                  </div>
-                  <MDBInput
-                    label='Rut'
-                    group
-                    type='text'
-                    validate
-                    labelClass='white-text'
-                  />
-                  <MDBInput
-                    label='Contraseña'
-                    group
-                    type='password'
-                    validate
-                    labelClass='white-text'
-                  />
-                  <div className='md-form pb-3'>
-                    <MDBInput
-                      label={
-                        <>
-                          Acepta los&nbsp;
-                          <a href='#!' className='green-text font-weight-bold'>
-                            Terminos y condiciones 
-                          </a>
-                        </>
-                      }
-                      type='checkbox'
-                      id='checkbox1'
-                      labelClass='white-text'
-                    />
-                  </div>
-                  <MDBRow className='d-flex align-items-center mb-4'>
-                    <div className='text-center mb-3 col-md-12'>
-                      <MDBBtn
-                        color='success'
-                        rounded
-                        type='button'
-                        className='btn-block z-depth-1'
-                      >
-                        Sign in
-                      </MDBBtn>
-                    </div>
-                  </MDBRow>
-                  <MDBCol md='12'>
-                    <p className='font-small white-text d-flex justify-content-end'>
-                      Have an account?
-                      <a href='#!' className='green-text ml-1 font-weight-bold'>
-                        Log in
-                      </a>
-                    </p>
-                  </MDBCol>
-                </div>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </center>
->>>>>>> 4bd157c00e9d2b0cceac935491d8b1364169a52a
     </div>
   );
   }
