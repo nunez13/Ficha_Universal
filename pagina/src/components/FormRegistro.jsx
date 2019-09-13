054aed4ae77ec1d81e4beadfdf1ab26879da356c
Patricio <pmardones2016@alu.uct.cl>
2019-09-13 19:50:21 -0300

    Modificacion Interfaz Registro

--- pagina/src/components/FormRegistro.jsx
+++ pagina/src/components/FormRegistro.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput } from 'mdbreact';
-
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput} from 'mdbreact';
+import Select from 'react-select'
 export default class FormRegistro extends Component {
 
   handleChange = ({target}) => {
@@ -32,31 +32,115 @@ export default class FormRegistro extends Component {
   render(){
     this.createUser()
     
-    return(
-      <form onSubmit={this.sendData}>
-        <label >Rut</label>
-        <input type="text" name="rut" onChange={this.handleChange}/><br/>
-        <label htmlFor="">Nombre</label>
-        <input type="text" name="name" onChange={this.handleChange}/><br/>
-        <label>Apellido</label>
-        <input type="text" name="lastName" onChange={this.handleChange}/><br/>
-        <label htmlFor="" >Email</label>
-        <input type="text" name="email" onChange={this.handleChange}/><br/>
-        <label htmlFor=""> Password</label>
-        <input type="password" placeholder="*****" name="password" onChange={this.handleChange}/><br/>
-        <label htmlFor="">NºColegioMedico</label>
-        <input type="text" name="nCollegeMedical" onChange={this.handleChange}/><br/>
-        <label htmlFor="">Gender</label>
-        <select name="gender" id="">
-          <option value="">Seleccione Una Opcion</option>
-          <option value="Masculino" onChange={this.handleChange}>Masculino </option>
-          <option value="Femenino"  onChange={this.handleChange}>Femenino</option>
-        </select><br/>
-        <label htmlFor="">Profession</label>
-        <input type="text" name="profession" onChange={this.handleChange}/><br/>
-        <input type="submit" value="Enviar"/>
-      </form>
-      
+return(
+      <div className="bg">
+      <center>
+        <MDBContainer>
+          <MDBRow style={{top:"50%"}}>
+            <MDBCol md='12'>
+              <MDBCard className='card-image' style={{backgroundImage:'url(http://www.wallpaperk.com/wallpapers/envelope-minimal-blue-7837.jpg)', width: '28rem'}}>
+                <div className='text-white rgba-stylish-strong py-5 px-5 z-depth-4'>
+                  <div className='text-center'>
+                    <h3 className='white-text mb-5 mt-4 font-weight-bold'>
+                      <strong>REGISTRO</strong>
+                      <strong className='green-text font-weight-bold'> MEDICO</strong>
+                    </h3>
+                  </div>
+                  <MDBInput
+                    label='Rut'
+                    group
+                    type='number'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="rut"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+                  <MDBInput
+                    label='Nombre'
+                    group
+                    type='text'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="name"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+                  <MDBInput
+                    label='Apellido'
+                    group
+                    type='text'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="lastName"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+                  <MDBInput
+                    label='Email'
+                    group
+                    type='email'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="email"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+                  <MDBInput
+                    label='Contraseña'
+                    group
+                    type='password'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="password"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+                  <MDBInput
+                    label='NºColegioMedico'
+                    group
+                    type='number'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="nCollegeMedical"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+                  <select className="browser-default custom-select" id="gender">
+                    <option>Selecciona tu genero</option>
+                    <option value="1">Masculino</option>
+                    <option value="2">Femenino</option>
+                  </select>
+                   <MDBInput
+                    label='Profesion'
+                    group
+                    type='text'
+                    validate
+                    labelClass='white-text'
+                    onChange={this.handleChange}
+                    id="profession"
+                    className='white-text mb-5 mt-4 font-weight-bold'
+                  />
+
+                  <MDBRow className='d-flex align-items-center mb-4'>
+                    <div className='text-center mb-3 col-md-12'>
+                      <MDBBtn
+                        color='success'
+                        rounded
+                        type='button'
+                        className='btn-block z-depth-1'
+                      >
+                        Registrar
+                      </MDBBtn>
+                    </div>
+                  </MDBRow>
+                </div>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </center>
+    </div>
     )
   }
 }
